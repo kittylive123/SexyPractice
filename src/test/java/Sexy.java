db6c47ad9577a07852ffe237e592fefdd3a37ea2
Alex <kittylive123>
2019-06-16 18:32:06 -0500

    i did something

--- src/test/java/Sexy.java
+++ src/test/java/Sexy.java
@@ -1,10 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import utilities.Driver;
 
+import java.util.List;
+
 public class Sexy {
 
-@Test
-    public void test(){
-    Driver.getDriver().get("https://www.booking.com");
-}
+    @Test
+    public void test() {
+        Driver.getDriver().get("https://www.booking.com/");
+
+    }
 }
